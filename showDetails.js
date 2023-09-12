0910ac805094fae98bd08bf35ae35d08a90ae418
Tapos Chandro <taposchandro231@gmail.com>
2023-09-12 21:31:32 +0600

    update deal section

---
+++ showDetails.js
@@ -0,0 +1,13 @@
+
+function productOne(id) {
+    const product = document.getElementById(id).style.display = 'block'
+}
+function productTwo(id) {
+    document.getElementById(id).style.display = 'block'
+}
+function productThree(id) {
+    document.getElementById(id).style.display = 'block'
+}
+function productFor(id) {
+    document.getElementById(id).style.display = 'block'
+}
